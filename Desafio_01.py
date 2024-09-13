2a66bb56f8affe14a58ab31af23c3244eaed814b
Harold Navarro <kaendor50@gmail.com>
2024-09-12 21:58:17 -0600

    Desafio 01 completado

---
+++ Desafio_01.py
@@ -0,0 +1,5 @@
+# Recibir el nombre del usuario y le de un saludo y nos retorne la longitud de la cadena junto con el saludo y nombre
+
+name = input('Escribe tu nombre => ')
+greet = f'Hola, {name}, como estas el dia de hoy?'
+print(f'{greet} Sabias que tu nombre tiene {len(name)} letras? es muy interesante')
